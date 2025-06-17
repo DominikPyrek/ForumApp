3cf459fe130777d98059b68c16d1199266a1e28a
DominikPyrek <pyrekdominik2025@gmail.com>
2025-06-17 13:16:00 +0200

    Created urls

--- Backend/Forum/forum/urls.py
+++ Backend/Forum/forum/urls.py
@@ -1,4 +1,26 @@
 from django.urls import path
-urlpatterns = [
+from .views import (
+    CreateUserAPIView,
+    UserAPIView,
+    CreatePostAPIView,
+    PostAPIView,
+    PostsAPIView,
+    CreateCommentAPIView,
+    CommentAPIView,
+    CommentsAPIView
+)
 
+
+urlpatterns = [
+    #User
+    path('users/', CreateUserAPIView.as_view(),  name='user-create'),
+    path('users/<int:pk>', UserAPIView.as_view(), name='user-detail'),
+    #Post
+    path('posts/', CreatePostAPIView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostAPIView.as_view(), name='post-detail'),
+    path('posts/list/', PostsAPIView.as_view(), name='post-list'),
+    #Comment
+    path('comments/', CreateCommentAPIView.as_view(), name='comment-create'),
+    path('comments/<int:pk>/', CommentAPIView.as_view(), name='comment-detail'),
+    path('comments/list/', CommentsAPIView.as_view(), name='comment-list'),
 ]
